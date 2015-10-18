9978093649e81de615d61de9bb7f5775a6f5f1f7
mohammadKarimi <dot.dotbydot@gmail.com>
2015-10-18 11:29:19 +0330

    add password Strongly

---
+++ YekanPedia.MaterialDesign.Console/src/Scripts/jQuery/PasswordStrongly/PasswordStrongly.js
@@ -0,0 +1,19 @@
+﻿//http://jsfiddle.net/aleem/KE3RB/8/
+$().ready(function () {
+    $('.PasswordStrongly').keyup(function (e) {
+        var strongRegex = new RegExp("^(?=.{8,})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*\\W).*$", "g");
+        //var mediumRegex = new RegExp("^(?=.{7,})(((?=.*[A-Z])(?=.*[a-z]))|((?=.*[A-Z])(?=.*[0-9]))|((?=.*[a-z])(?=.*[0-9]))).*$", "g");
+        //var enoughRegex = new RegExp("(?=.{6,}).*", "g");
+        if (strongRegex.test($(this).val())) {
+            $('.has-passwordStrongly').removeClass('has-warning');
+            $(".has-passwordStrongly .help-block").html('');
+            $(".has-passwordStrongly .form-control-feedback").removeClass('zmdi-alert-triangle');
+
+        } else {
+            $('.has-passwordStrongly').addClass('has-warning');
+            $(".has-passwordStrongly .help-block").html('امنیت کلمه عبورانتخابی پایین است');
+            $(".has-passwordStrongly .form-control-feedback").addClass('zmdi-alert-triangle');
+        }
+        return true;
+    });
+});
